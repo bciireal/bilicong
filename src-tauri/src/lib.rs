90415ddb9fa7d17deb4e97c410be0fc12e7fc06a
bciireal <188691417+bciireal@users.noreply.github.com>
2026-04-04 23:15:01 +0800

    chore: enable log print in backend

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,6 +4,22 @@ mod adb;
 mod entries;
 mod mix_media;
 
+#[cfg(debug_assertions)]
+fn log_init() {
+    use tracing_subscriber::filter::{EnvFilter, LevelFilter};
+
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::WARN.into())
+                .from_env_lossy(),
+        )
+        .init();
+}
+
+#[cfg(not(debug_assertions))]
+fn log_init() {}
+
 /// Run tauri ui
 /// # Panics
 /// Panics if tauri app faild to run
@@ -12,6 +28,10 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
+        .setup(|_| {
+            log_init();
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![
             adb::get_devices,
             adb::get_all_pages,
